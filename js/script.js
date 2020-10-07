f31c48b30ea82a3b27670d46c63342e38128d640
Dumuch <dumuch@yandex.ru>
2020-10-07 22:08:09 +0800

    Карточки открываются по индексу

--- js/script.js
+++ js/script.js
@@ -76,17 +76,59 @@ var fragment = document.createDocumentFragment();
 // запускаем цикл
 for (var i = 0; i < pictureElements.length; i++) {
   // в fragment вставляем шаблоны
-  linkPicture.id = 'picture-' + i;
+  linkPicture.id = i;
 
   fragment.appendChild(renderPicture(pictureElements[i]));
 };
 
 uploadFormPreview.appendChild(fragment);
 
-var pictureId = uploadFormPreview.querySelector('.picture');
-console.log(uploadFormPreview);
-console.log(pictureId);
-pictureId.addEventListener('click', function() {
-  galleryOverlay.classList.remove('hidden');
 
-});
+var galleryOverlay = document.querySelector('.gallery-overlay');
+var galleryOverlayImage = galleryOverlay.querySelector('.gallery-overlay-image');
+
+var likesCount = galleryOverlay.querySelector('.likes-count');
+var commentsCount = galleryOverlay.querySelector('.comments-count');
+
+var galleryOverlayCommentsList = galleryOverlay.querySelector('.gallery-overlay-comments-list');
+var socialCommentText1 = document.querySelector('.social__comment').content;
+
+
+var renderGalleryOverlay = function(pictureElements) {
+  // клонируем наш шаблон
+  // подменяем контент в setup-similar-label из объекта
+  galleryOverlayImage.src = 'img/' + pictureElements.url;
+  likesCount.textContent = pictureElements.likes;
+  commentsCount.textContent = pictureElements.comments.length;
+};
+
+var pictureId = uploadFormPreview.querySelectorAll('.picture');
+
+function topTop(evt) {
+  galleryOverlay.classList.remove('hidden');
+  var pictureIdIndex = evt.currentTarget.id;
+
+  var comments1 = pictureElements[pictureIdIndex];
+  var comments2 = comments1.comments;
+  var len = comments2.length;
+
+  var galleryOverlayCommentsText = function(pictureElements, indexComment) {
+    var socialCommentElement = socialCommentText1.cloneNode(true);
+    socialCommentElement.querySelector('.social__text').textContent = indexComment; // возвращаем глобальную переменную
+    return socialCommentElement;
+  };
+
+  var fragmentOverlay = document.createDocumentFragment();
+  for (var i = 0; i < len; i++) {
+    var indexComment = comments2[i];
+    fragmentOverlay.appendChild(galleryOverlayCommentsText(pictureElements[i], indexComment));
+  };
+
+  galleryOverlayCommentsList.appendChild(fragmentOverlay);
+  renderGalleryOverlay(pictureElements[pictureIdIndex]);
+};
+
+for (var i = 0; i < pictureId.length; i++) {
+  pictureId[i].addEventListener('click', topTop, false);
+
+};
